246c9397f971df27d794df661dabed17f0ff4f52
haeseung <showui96@gmail.com>
2023-10-17 15:44:22 +0900

    add: users, apply module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CompaniesController } from './companies/companies.controller';
-import { CompaniesModule } from './companies/companies.module';
+import { CompaniesModule } from './companies/module/companies.module';
 import { CompanyEntity } from './companies/entities/companies.entity';
-import { CompaniesService } from './companies/companies.service';
+import { RecruitsModule } from './recruits/module/recruits.module';
+import { RecruitEntity } from './recruits/entities/recruits.entity';
+import { UsersModule } from './users/module/users.module';
+import { UserEntity } from './users/entities/users.entity';
+import { ApplyModule } from './apply/module/apply.module';
+import { ApplyEntity } from './apply/entities/apply.entity';
+
 
 @Module({
   imports: [
@@ -23,13 +28,16 @@ import { CompaniesService } from './companies/companies.service';
         database: configService.get('DB_NAME'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
-        entities: [CompanyEntity],
-        synchronize: false,
+        entities: [CompanyEntity, RecruitEntity, UserEntity, ApplyEntity],
+        synchronize: true,
         logging: true,
         timezone: 'local',
       })
     }),
-    CompaniesModule
+    UsersModule,
+    CompaniesModule,
+    RecruitsModule,
+    ApplyModule
   ],
   controllers: [AppController],
   providers: [AppService],
